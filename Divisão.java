34831552b6521ea972e9cca597327c1900543149
William Nogueira Borba <71046914+WilliamBorba99@users.noreply.github.com>
2021-03-26 19:00:50 -0300

    Add files via upload

---
+++ Divisão.java
@@ -0,0 +1,31 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Divisão {
+	public static void main (String[] args)
+	{
+		double resultado;
+		int valor1, valor2;
+		String retorno;
+		
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("Digite o primeiro valor: ");
+		valor1 = scanner.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		valor2 = scanner.nextInt();
+		
+		if (valor2 == 0) {
+			 do {
+				 System.out.println("O segundo valor não pode ser igual a zero, pro favor digite novamente: ");
+				 valor2 = scanner.nextInt(); 				 
+			 }while (valor2 == 0);
+			 
+		}  
+		resultado = valor1 / valor2;
+		retorno = valor1 + "/" + valor2 + "=" + resultado;
+		System.out.println(retorno);
+				
+	}
+}
---
+++ Media.java
@@ -0,0 +1,33 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Media {
+	public static void main (String[] args)
+	{
+		double media;
+		int nota1, nota2, nota3;
+		String retorno;
+		
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("Digite sua primeira nota: ");
+		nota1 = scanner.nextInt();
+		System.out.println("Digite sua segunda nota: ");
+		nota2 = scanner.nextInt();
+		System.out.println("Digite sua terceira nota: ");
+		nota3 = scanner.nextInt();	
+		
+		media = (nota1 + nota2 + nota3) / 3;
+		if (media > 7)
+			System.out.println("Aprovado");
+		else
+			System.out.println("Reprovado");
+		
+		media = (nota1 + nota2 + nota3) / 3;
+		retorno = nota1 + "+" + nota2 + "+" + nota3 + "/ 3 ="+ media;
+		System.out.println(retorno);
+						
+	}
+
+}
